63635a7b7dd84810e3707b3c1077d3e59f506d1e
NhanNgo2306 <45350543+NhanNgo2306@users.noreply.github.com>
2019-07-18 10:48:53 +0700

    delete

--- environment-base.sh
+++
@@ -1,8 +0,0 @@
-YELLOW='\033[1;33m'
-NC='\033[0m'
-echo "${YELLOW}--------Install OpenSSL${NC}"
-sudo apt-get install openssl
-echo "${YELLOW}--------Installing nano${NC}"
-sudo apt-get install nano
-echo "${YELLOW}--------Installing v4l${NC}"
-sudo apt-get install v4l-utils 
